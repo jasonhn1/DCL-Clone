f539b13ae51877f962d23bdf8c37d1f407350657
tuan <tuan0919@gmail.com>
2023-04-06 23:04:23 -0700

    add category and skeleton code

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function loadImages() {
+    // get query param value for "type"
+    // load the data.json file
+    // cast the file data into map
+    // get data where the key is the value for "type" above.
+    // generate the repeat elements for each image url in the map
+    // find the div with the id "image-container"
+    // add the newly generated elements into that div
+    // call lazyload function to init the image loading when image is in the view port (ie lazyload())
+}
